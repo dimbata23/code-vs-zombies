d7e0a88105030e24f6d81a8907a85e6a4c89b49b
dimbata23 <adimitrov23@outlook.com>
2024-06-08 00:43:26 +0300

    basic simulation -> prediction

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // www.codingames.com supports one file only :(
 
+use std::cmp::PartialEq;
 use std::fmt::{Display, Formatter};
 use std::io;
 
@@ -7,6 +8,8 @@ use std::io;
  * Save humans, destroy zombies!
  **/
 fn main() {
+    let mut last_prediction = None;
+
     // game loop
     loop {
         let state = GameState {
@@ -14,7 +17,14 @@ fn main() {
             humans: parse_humans(),
             zombies: parse_zombies(),
         };
-        eprintln!("{:?}", state);
+
+        eprintln!("Current : {}", state);
+        if let Some(pred_state) = &last_prediction {
+            eprintln!("Pred was: {}", *pred_state);
+            eprintln!("{}", *pred_state == state);
+        }
+
+        last_prediction = Some(state.simulate(calculate_next_move));
 
         let next_move = calculate_next_move(&state);
         println!("{}", next_move)
@@ -42,11 +52,21 @@ impl Prediction {
 
 // ----- Game State -----
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 struct GameState {
     player: Player,
     humans: Vec<Human>,
     zombies: Vec<Zombie>,
+}
+
+impl Display for GameState {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut res = write!(f, "P: ({}, {}), {}H, {}Z: ", self.player.x, self.player.y, self.humans.len(), self.zombies.len());
+        for zombie in &self.zombies {
+            res = res.and(write!(f, "{} ", zombie));
+        }
+        res
+    }
 }
 
 impl GameState {
@@ -56,15 +76,38 @@ impl GameState {
 
     fn simulate(&self, strategy: fn(&GameState) -> Player) -> GameState {
         let mut next_state = self.clone();
-        next_state.player = strategy(&self);
-        // TODO: make calculations
+        next_state.move_zombies();
+        let player_target = strategy(&self);
+        (next_state.player.x, next_state.player.y) = move_from_to_capped((self.player.x, self.player.y), (player_target.x, player_target.y), PLAYER_STEP);
+        next_state.kill_zombies();
+        next_state.kill_humans();
+        next_state.calc_zombies_next_move();
         next_state
     }
 
-    // TODO: test
-    fn calc_zombie_targets(&mut self) {
-        // TODO: fix mutable/immutable borrows
-        self.zombies.iter_mut().for_each(|x| x.set_closest_human(&self));
+    fn calc_zombies_next_move(&mut self) {
+        let mut new_zombies = self.zombies.clone();  // old compilers :((((
+        new_zombies.iter_mut().for_each(|x| x.set_target(&self.player, &self.humans));
+        new_zombies.iter_mut().for_each(|z| z.set_next_move(&self.player, &self.humans));
+        self.zombies = new_zombies;
+    }
+
+    fn move_zombies(&mut self) {
+        for zombie in &mut self.zombies {
+            (zombie.x, zombie.y) = (zombie.next_x, zombie.next_y);
+        }
+    }
+
+    fn kill_zombies(&mut self) {
+        let mut new_zombies = self.zombies.clone();  // old compilers :((((
+        new_zombies.iter_mut().for_each(|z| z.check_within_player(&self.player));
+        self.zombies = new_zombies.iter().filter(|z| !z.dead).cloned().collect();
+    }
+
+    fn kill_humans(&mut self) {
+        let mut new_humans = self.humans.clone();  // old compilers :((((
+        new_humans.iter_mut().for_each(|h| h.check_within_zombie(&self.zombies));
+        self.humans = new_humans.iter().filter(|h| !h.dead).cloned().collect();
     }
 }
 
@@ -72,14 +115,18 @@ impl GameState {
 
 // ----- Move logic -----
 fn calculate_next_move(state: &GameState) -> Player {
-    return Player::new(state.humans[0].x, state.humans[0].y)
+    //Player::new(0, 0)
+    Player::new(state.humans[0].x, state.humans[0].y)
 }
 
 
 
 // ----- Player -----
 
-#[derive(Debug, Clone)]
+const PLAYER_RANGE: i32 = 2000;
+const PLAYER_STEP: i32 = 1000;
+
+#[derive(Debug, Clone, PartialEq)]
 struct Player {
     x: i32,
     y: i32,
@@ -115,11 +162,12 @@ impl Display for Player {
 
 // ----- Humans -----
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 struct Human {
     id: i32,
     x: i32,
     y: i32,
+    dead: bool,
 }
 
 impl Human {
@@ -129,7 +177,12 @@ impl Human {
             id: input[0],
             x: input[1],
             y: input[2],
+            dead: false,
         }
+    }
+
+    fn check_within_zombie(&mut self, zombies: &[Zombie]) {
+        self.dead = zombies.iter().any(|z| (z.x, z.y) == (self.x, self.y));
     }
 }
 
@@ -147,6 +200,8 @@ fn parse_humans() -> Vec<Human> {
 
 // ----- Zombies -----
 
+const ZOMBIE_STEP: i32 = 400;
+
 #[derive(Debug, Copy, Clone)]
 struct Zombie {
     id: i32,
@@ -155,6 +210,19 @@ struct Zombie {
     next_x: i32,
     next_y: i32,
     target: Target,
+    dead: bool,
+}
+
+impl Display for Zombie {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{id: {}, pos: ({}, {}), next: ({}, {})}}", self.id, self.x, self.y, self.next_x, self.next_y)
+    }
+}
+
+impl PartialEq for Zombie {
+    fn eq(&self, other: &Self) -> bool {
+        (self.id, self.x, self.y, self.next_x, self.next_y) == (other.id, other.x, other.y, other.next_x, other.next_y)
+    }
 }
 
 impl Zombie {
@@ -167,19 +235,33 @@ impl Zombie {
             next_x: input[3],
             next_y: input[4],
             target: Target::Player,
+            dead: false,
         }
     }
 
-    fn set_closest_human(&mut self, state: &GameState) {
-        let mut sq_dist = dist_squared(self.next_x, self.next_y, state.player.x, state.player.y);
+    fn set_target(&mut self, player: &Player, humans: &[Human]) {
+        let mut sq_dist = dist_squared((self.next_x, self.next_y), (player.x, player.y));
         self.target = Target::Player;
-        for (idx, human) in state.humans.iter().enumerate() {
-            let curr_dist = dist_squared(self.next_x, self.next_y, human.x, human.y);
+        for (idx, human) in humans.iter().enumerate() {
+            let curr_dist = dist_squared((self.next_x, self.next_y), (human.x, human.y));
             if curr_dist < sq_dist {
                 sq_dist = curr_dist;
                 self.target = Target::Human(idx);
             }
         }
+    }
+
+    fn check_within_player(&mut self, player: &Player) {
+        self.dead = dist_squared((self.next_x, self.next_y), (player.x, player.y)) <= PLAYER_RANGE * PLAYER_RANGE;
+    }
+
+    fn set_next_move(&mut self, player: &Player, humans: &[Human]) {
+        self.set_target(player, humans);
+        let (mut target_x, mut target_y) = (player.x, player.y);
+        if let Target::Human(idx) = self.target {
+            (target_x, target_y) = (humans[idx].x, humans[idx].y);
+        }
+        (self.next_x, self.next_y) = move_from_to_capped((self.next_x, self.next_y), (target_x, target_y), ZOMBIE_STEP);
     }
 }
 
@@ -193,7 +275,7 @@ fn parse_zombies() -> Vec<Zombie> {
     res
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Target {
     Player,         // the player
     Human(usize),   // human idx
@@ -219,10 +301,36 @@ fn read_line_as_i32() -> i32 {
     atoi(&input_line)
 }
 
-fn dist_squared(x1: i32, y1: i32, x2: i32, y2: i32) -> i32 {
-    (x1-x2) * (x1-x2) + (y1-y2) * (y1-y1)
+fn dist_squared((x1, y1): (i32, i32), (x2, y2): (i32, i32)) -> i32 {
+    (x1-x2) * (x1-x2) + (y1-y2) * (y1-y2)
 }
 
-fn dist(x1: i32, y1: i32, x2: i32, y2: i32) -> f32 {
-    (dist_squared(x1, y1, x2, y2) as f32).sqrt()
+fn dist((x1, y1): (i32, i32), (x2, y2): (i32, i32)) -> f32 {
+    (dist_squared((x1, y1), (x2, y2)) as f32).sqrt()
+}
+
+fn move_from_to_capped((x1, y1): (i32, i32), (x2, y2): (i32, i32), cap: i32) -> (i32, i32) {
+    if dist((x1, y1), (x2, y2)) <= cap as f32 {
+        return (x2, y2);
+    }
+
+    let dir = scale(norm(vec((x1, y1), (x2, y2))), cap);
+    add((x1, y1), dir)
+}
+
+fn vec((x1, y1): (i32, i32), (x2, y2): (i32, i32)) -> (i32, i32) {
+    (x2 - x1, y2 - y1)
+}
+
+fn norm((x, y): (i32, i32)) -> (f32, f32) {
+    let len = ((x*x + y*y) as f32).sqrt();
+    (x as f32 / len, y as f32 / len)
+}
+
+fn scale((x, y): (f32, f32), scalar: i32) -> (f32, f32) {
+    (x * scalar as f32, y * scalar as f32)
+}
+
+fn add((x1, y1): (i32, i32), (x2, y2): (f32, f32)) -> (i32, i32) {
+    (((x1 as f32) + x2) as i32, ((y1 as f32) + y2) as i32)
 }
